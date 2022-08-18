66f8b024ea5eeab4362965b34e069435804160dc
leirock <19180725+leirock@users.noreply.github.com>
2022-08-18 19:05:26 +0800

    Update post meta

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ hexo.extend.filter.register('theme_inject', (injects) => {
   const config = utils.defaultConfigFile('artalk', 'default.yaml');
   if (!config.enable) return;
 
-  injects.comment.raw('artalk', '<div class="comments" id="artalk"></div>', {}, { cache: true });
+  injects.comment.raw('artalk', '<div class="comments" id="comments"></div>', {}, { cache: true });
   injects.bodyEnd.raw('artalk', utils.getFileContent('artalk.njk'));
   injects.head.raw('artalk', `<link rel="preconnect" href="${config.server}">`, {}, {});
 });
@@ -34,22 +34,6 @@ hexo.extend.filter.register('theme_inject', (injects) => {
   if (!config.enable || !config.server) return;
 
     injects.postMeta.raw(
-      'artalk_comment_count',
-      `
-        {% if post.comments and config.artalk.commentCount %}
-        <span class="post-meta-item">
-          ${iconText('far fa-comment', 'artalk')}
-          <a title="artalk" href="{{ url_for(post.path) }}#artalk" itemprop="discussionUrl">
-            <span id="ArtalkCount" class="post-comments-count" data-page-key="{{ url_for(post.path) }}" itemprop="commentCount"></span>
-          </a>
-        </span>
-        {% endif %}
-      `,
-      {},
-      {}
-    );
-
-    injects.postMeta.raw(
       'artalk_pv_count',
       `
         {% if config.artalk.pvCount %}
@@ -59,6 +43,22 @@ hexo.extend.filter.register('theme_inject', (injects) => {
           </span>
           <span class="post-meta-item-text">{{ __('post.views') + __('symbol.colon') }}</span>
           <span id="ArtalkPV" data-page-key="{{ url_for(post.path) }}" ></span>
+        </span>
+        {% endif %}
+      `,
+      {},
+      {}
+    );
+
+    injects.postMeta.raw(
+      'artalk_comment_count',
+      `
+        {% if post.comments and config.artalk.commentCount %}
+        <span class="post-meta-item">
+          ${iconText('far fa-comment', 'artalk')}
+          <a href="{{ url_for(post.path) }}#comments" itemprop="discussionUrl">
+            <span id="ArtalkCount" class="post-comments-count" data-page-key="{{ url_for(post.path) }}" itemprop="commentCount"></span>
+          </a>
         </span>
         {% endif %}
       `,
