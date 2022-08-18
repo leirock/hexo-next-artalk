878b6c936cb072db015ca77bf4750b9583f0a4d7
Dylan Zhang <19180725+leirock@users.noreply.github.com>
2022-08-19 02:55:25 +0800

    Update post meta (#5)
    * Update post meta
    * Edit README
    * Edit post meta text settings

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # Artalk for Hexo NexT
 
+> [中文](https://github.com/leirock/hexo-next-artalk/blob/main/README-CN.md)
+
 [![NPM version](https://img.shields.io/npm/v/hexo-next-artalk?color=red&logo=npm&style=flat-square)](https://www.npmjs.com/package/hexo-next-artalk)
 [![NPM downloads](https://img.shields.io/npm/dm/hexo-next-artalk?color=red&logo=npm&style=flat-square)](https://www.npmjs.com/package/hexo-next-artalk)
 [![Theme Version](https://img.shields.io/badge/NexT-v7.3.0+-blue?style=flat-square)](https://github.com/next-theme/hexo-theme-next)
 [![Artalk Version](https://img.shields.io/badge/Artalk-v2.3.1+-558fb5?style=flat-square)](https://github.com/ArtalkJS/Artalk)
 
-[Artalk](https://artalk.js.org) comment plugin for Hexo NexT.
+This is an [Artalk](https://artalk.js.org) comment plugin for Hexo NexT.
 
 ## Install
 
@@ -15,29 +17,28 @@ npm install hexo-next-artalk
 
 ## Configure
 
-Set the value `enable` to `true`, add `server`, and edit other configurations in `artalk` section in the config file as following. You can config those in Hexo's root config file `_config.yml`:
+Add the following configurations in Hexo's root config file `_config.yml`, set `enable` to `true`, and edit other configurations in `artalk` section as your own values.
 
 ```yaml
 # Artalk Config File
-# For more information: https://artalk.js.org
+## For more information: https://artalk.js.org
 
 artalk:
   enable: false
 
-  # Server address url, you should set this to your own link
+  # Backend server API, you should set this to your own server API
   server: https://artalk.example.com
 
   # Site name, you can set this to specify site for server
   # or leave blank to use default site
   site:
 
-  # JS CDN url, you can set this to your preferred CDN
+  # Frontend resources CDN, you can set this to your preferred CDN
+  ## Reference: https://artalk.js.org/guide/frontend/install.html
   jsUrl: https://unpkg.com/artalk@2/dist/Artalk.js
-
-  # CSS styles CDN url, you can set this to your preferred CDN
   cssUrl: https://unpkg.com/artalk@2/dist/Artalk.css
 
-  # PV count
+  # Page views count
   pvCount: true
 
   # Comments count
@@ -47,5 +48,20 @@ artalk:
   darkMode: auto
 
   # Use backend to config
-  # useBackendConf: true
+  ## Reference: https://artalk.js.org/guide/backend/fe-control.html
+  useBackendConf: false
+```
+
+## Tips
+
+The default text of comments count in post meta is "Artalk". If you would like to change it, for example, to simplified Chinese "评论数":
+
+1. Create a file `/source/_data/languages.yml` in your Hexo blog folder;
+2. Edit this file as follows and then save it.
+
+```yaml
+zh-CN:
+  post:
+    comments:
+      artalk: 评论数
 ```
--- index.js
+++ index.js
@@ -10,39 +10,27 @@ hexo.extend.filter.register('theme_inject', (injects) => {
   const config = utils.defaultConfigFile('artalk', 'default.yaml');
   if (!config.enable) return;
 
-  injects.comment.raw('artalk', '<div class="comments" id="artalk"></div>', {}, { cache: true });
+  injects.comment.raw('artalk', '<div class="comments" id="comments"></div>', {}, { cache: true });
   injects.bodyEnd.raw('artalk', utils.getFileContent('artalk.njk'));
   injects.head.raw('artalk', `<link rel="preconnect" href="${config.server}">`, {}, {});
 });
 
 // Add post_meta
-const capitalize = (input) => input.toString().charAt(0).toUpperCase() + input.toString().substr(1);
-const iconText = (icon, key, defaultValue = capitalize(key)) =>
-  `
-    <span class="post-meta-item-icon">
-      <i class="${icon}"></i>
-    </span>
-    {%- set post_meta_comment = __('post.comments.${key}') %}
-    {%- if post_meta_comment == 'post.comments.${key}' %}
-      {%- set post_meta_comment = '${defaultValue}' %}
-    {%- endif %}
-    <span class="post-meta-item-text">{{ post_meta_comment + __('symbol.colon') }}</span>
-  `;
-
 hexo.extend.filter.register('theme_inject', (injects) => {
   const config = utils.defaultConfigFile('artalk', 'default.yaml');
   if (!config.enable || !config.server) return;
 
     injects.postMeta.raw(
-      'artalk_comment_count',
+      'artalk_pv_count',
       `
-        {% if post.comments and config.artalk.commentCount %}
-        <span class="post-meta-item">
-          ${iconText('far fa-comment', 'artalk')}
-          <a title="artalk" href="{{ url_for(post.path) }}#artalk" itemprop="discussionUrl">
-            <span id="ArtalkCount" class="post-comments-count" data-page-key="{{ url_for(post.path) }}" itemprop="commentCount"></span>
-          </a>
-        </span>
+        {% if config.artalk.pvCount %}
+          <span class="post-meta-item" title="{{ __('post.views') }}">
+            <span class="post-meta-item-icon">
+              <i class="far fa-eye"></i>
+            </span>
+            <span class="post-meta-item-text">{{ __('post.views') + __('symbol.colon') }}</span>
+            <span id="ArtalkPV" data-page-key="{{ url_for(post.path) }}" ></span>
+          </span>
         {% endif %}
       `,
       {},
@@ -50,16 +38,22 @@ hexo.extend.filter.register('theme_inject', (injects) => {
     );
 
     injects.postMeta.raw(
-      'artalk_pv_count',
+      'artalk_comment_count',
       `
-        {% if config.artalk.pvCount %}
-        <span class="post-meta-item" title="{{ __('post.views') }}">
-          <span class="post-meta-item-icon">
-            <i class="far fa-eye"></i>
+        {% if post.comments and config.artalk.commentCount %}
+          {%- set post_meta_comment = __('post.comments.artalk') %}
+          {%- if post_meta_comment == 'post.comments.artalk' %}
+            {%- set post_meta_comment = 'Artalk' %}
+          {%- endif %}
+          <span class="post-meta-item" title="{{ post_meta_comment }}">
+            <span class="post-meta-item-icon">
+              <i class="far fa-comment"></i>
+            </span>
+            <span class="post-meta-item-text">{{ post_meta_comment + __('symbol.colon') }}</span>
+            <a href="{{ url_for(post.path) }}#comments" itemprop="discussionUrl">
+              <span id="ArtalkCount" class="post-comments-count" data-page-key="{{ url_for(post.path) }}" itemprop="commentCount"></span>
+            </a>
           </span>
-          <span class="post-meta-item-text">{{ __('post.views') + __('symbol.colon') }}</span>
-          <span id="ArtalkPV" data-page-key="{{ url_for(post.path) }}" ></span>
-        </span>
         {% endif %}
       `,
       {},
