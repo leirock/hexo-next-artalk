5d17b001a5f0eed2f6e0cb2153d495a011e75e2a
leirock <19180725+leirock@users.noreply.github.com>
2022-08-19 02:50:48 +0800

    Edit post meta text settings

--- index.js
+++ index.js
@@ -16,19 +16,6 @@ hexo.extend.filter.register('theme_inject', (injects) => {
 });
 
 // Add post_meta
-const capitalize = (input) => input.toString().charAt(0).toUpperCase() + input.toString().substr(1);
-const iconText = (icon, key, defaultValue = capitalize(key)) =>
-  `
-    <span class="post-meta-item-icon">
-      <i class="${icon}"></i>
-    </span>
-    {%- set post_meta_comment = __('post.comments.${key}') %}
-    {%- if post_meta_comment == 'post.comments.${key}' %}
-      {%- set post_meta_comment = '${defaultValue}' %}
-    {%- endif %}
-    <span class="post-meta-item-text">{{ post_meta_comment + __('symbol.colon') }}</span>
-  `;
-
 hexo.extend.filter.register('theme_inject', (injects) => {
   const config = utils.defaultConfigFile('artalk', 'default.yaml');
   if (!config.enable || !config.server) return;
@@ -37,13 +24,13 @@ hexo.extend.filter.register('theme_inject', (injects) => {
       'artalk_pv_count',
       `
         {% if config.artalk.pvCount %}
-        <span class="post-meta-item" title="{{ __('post.views') }}">
-          <span class="post-meta-item-icon">
-            <i class="far fa-eye"></i>
+          <span class="post-meta-item" title="{{ __('post.views') }}">
+            <span class="post-meta-item-icon">
+              <i class="far fa-eye"></i>
+            </span>
+            <span class="post-meta-item-text">{{ __('post.views') + __('symbol.colon') }}</span>
+            <span id="ArtalkPV" data-page-key="{{ url_for(post.path) }}" ></span>
           </span>
-          <span class="post-meta-item-text">{{ __('post.views') + __('symbol.colon') }}</span>
-          <span id="ArtalkPV" data-page-key="{{ url_for(post.path) }}" ></span>
-        </span>
         {% endif %}
       `,
       {},
@@ -54,12 +41,19 @@ hexo.extend.filter.register('theme_inject', (injects) => {
       'artalk_comment_count',
       `
         {% if post.comments and config.artalk.commentCount %}
-        <span class="post-meta-item">
-          ${iconText('far fa-comment', 'artalk')}
-          <a href="{{ url_for(post.path) }}#comments" itemprop="discussionUrl">
-            <span id="ArtalkCount" class="post-comments-count" data-page-key="{{ url_for(post.path) }}" itemprop="commentCount"></span>
-          </a>
-        </span>
+          {%- set post_meta_comment = __('post.comments.artalk') %}
+          {%- if post_meta_comment == 'post.comments.artalk' %}
+            {%- set post_meta_comment = 'Artalk' %}
+          {%- endif %}
+          <span class="post-meta-item" title="{{ post_meta_comment }}">
+            <span class="post-meta-item-icon">
+              <i class="far fa-comment"></i>
+            </span>
+            <span class="post-meta-item-text">{{ post_meta_comment + __('symbol.colon') }}</span>
+            <a href="{{ url_for(post.path) }}#comments" itemprop="discussionUrl">
+              <span id="ArtalkCount" class="post-comments-count" data-page-key="{{ url_for(post.path) }}" itemprop="commentCount"></span>
+            </a>
+          </span>
         {% endif %}
       `,
       {},
